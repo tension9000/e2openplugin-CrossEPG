c1fe19601eb3860b1944a74d027f8d4d167773e2
tension <tension9000@yahoo.it>
2015-07-25 21:15:06 +0200

    Revert commit Do not wrrite next download time two times
    Do not wrrite next download time two times. It is already done
    in setup module. (reverted from commit 514fa4ce03952714e87fe5dc6d6d675c3093497d)

--- src/enigma2/python/crossepg_auto.py
+++ src/enigma2/python/crossepg_auto.py
@@ -144,6 +144,7 @@ class CrossEPG_Auto:
 			self.crossepgtimer.startLongTimer(next)
 		else:
 			CrossEPGTime = -1
+		self.config.next_update_time = CrossEPGTime
 		self.config.save()
 		print "[CrossEPG_Auto] Time set to", strftime("%c", localtime(CrossEPGTime)), strftime("(now=%c)", localtime(now))
 		return CrossEPGTime
